1196d5c5926b66627bf14f49c0497a4a84d5df1b
bobinskim <m.bobinski@stat.gov.pl>
2020-07-29 19:33:34 +0200

    update antora entrypoint

--- docs-source/antora-action-env/entrypoint.sh
+++ docs-source/antora-action-env/entrypoint.sh
@@ -1,6 +1,5 @@
-#!/bin/sh -l
+#!/bin/bash
 
-sh -c "Generating "
-sh -c "cd docs-source"
-sh -c "antora site.yml"
-sh -c "chmod a+xrw build/site"
+cd docs-source
+antora site.yml
+chmod a+xrw build/site
