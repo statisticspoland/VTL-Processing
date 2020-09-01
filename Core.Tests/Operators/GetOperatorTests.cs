77c6473a11aaf3cad43667bca8efa6c23630d9b3
sklodowskim <m.sklodowski@stat.gov.pl>
2020-09-01 10:33:00 +0200

    Unit tests - utilities

--- Core.Tests/Operators/GetOperatorTests.cs
+++ Core.Tests/Operators/GetOperatorTests.cs
@@ -1,20 +1,17 @@
 ﻿namespace StatisticsPoland.VtlProcessing.Core.Tests.Operators
 {
     using Moq;
+    using StatisticsPoland.VtlProcessing.Core.ErrorHandling;
     using StatisticsPoland.VtlProcessing.Core.Infrastructure;
     using StatisticsPoland.VtlProcessing.Core.Infrastructure.DependencyInjection;
-    using StatisticsPoland.VtlProcessing.Core.Models;
     using StatisticsPoland.VtlProcessing.Core.Models.Interfaces;
-    using StatisticsPoland.VtlProcessing.Core.Models.Logical;
-    using StatisticsPoland.VtlProcessing.Core.Models.Types;
     using StatisticsPoland.VtlProcessing.Core.Operators;
+    using StatisticsPoland.VtlProcessing.Core.Tests.Utilities;
     using System;
     using Xunit;
 
     public class GetOperatorTests
     {
-        private readonly ExpressionResolver exprResolver;
-        private readonly DataStructureResolver dsResolver;
         private readonly OperatorResolver opResolver;
         private readonly IDataModel dataModel;
 
@@ -25,19 +22,6 @@
 
             this.dataModel = dataModelMock.Object;
 
-            Mock<ExpressionResolver> exprResolverMock = new Mock<ExpressionResolver>();
-            exprResolverMock.Setup(o => o(null)).Returns(() => { return new Expression(); });
-            exprResolverMock.Setup(o => o(It.IsNotNull<IExpression>())).Returns((IExpression parentExpr) => { return new Expression(parentExpr); });
-
-            this.exprResolver = exprResolverMock.Object;
-
-            Mock<DataStructureResolver> dsResolverMock = new Mock<DataStructureResolver>();
-            dsResolverMock.Setup(o => o(null, null, null)).Returns(() => { return new DataStructure(); });
-            dsResolverMock.Setup(o => o(It.IsNotNull<string>(), It.IsNotNull<ComponentType?>(), It.IsNotNull<BasicDataType?>()))
-                .Returns((string compName, ComponentType? compType, BasicDataType? dataType) => { return new DataStructure(compName, (ComponentType)compType, (BasicDataType)dataType); });
-
-            this.dsResolver = dsResolverMock.Object;
-
             Mock<OperatorResolver> opResolverMock = new Mock<OperatorResolver>();
             opResolverMock.Setup(o => o("get")).Returns(() => { return new GetOperator(this.dataModel); });
 
@@ -47,8 +31,7 @@
         private IDataStructure GetDataStructure(string dsName)
         {
             if (dsName != "IntsDataset") throw new Exception();
-            IDataStructure ds = this.dsResolver("Id1", ComponentType.Identifier, BasicDataType.Integer);
-            ds.Measures.Add(new StructureComponent(BasicDataType.Integer, "Me1", ComponentType.Measure));
+            IDataStructure ds = TestExprFactory.GetExpression(TestExprType.IntsDataset).Structure;
             ds.DatasetName = dsName;
 
             return ds;
@@ -57,23 +40,20 @@
         [Fact]
         public void GetOutputStructure_CorrectExpr_DataStructure()
         {
-            IExpression getExpr = this.exprResolver();
+            IExpression getExpr = ModelResolvers.ExprResolver();
             getExpr.ExpressionText = "IntsDataset";
 
             getExpr.OperatorDefinition = this.opResolver("get");
 
-            IDataStructure expected = this.dsResolver("Id1", ComponentType.Identifier, BasicDataType.Integer);
-            expected.Measures.Add(new StructureComponent(BasicDataType.Integer, "Me1", ComponentType.Measure));
-
             IDataStructure dataStructure = getExpr.OperatorDefinition.GetOutputStructure(getExpr);
 
-            Assert.True(expected.EqualsObj(dataStructure));
+            Assert.True(TestExprFactory.GetExpression(TestExprType.IntsDataset).Structure.EqualsObj(dataStructure));
         }
 
         [Fact]
         public void GetOutputStructure_WrongExpr_ThrowsException()
         {
-            IExpression getExpr = this.exprResolver();
+            IExpression getExpr = ModelResolvers.ExprResolver();
             getExpr.ExpressionText = "Dataset";
 
             getExpr.OperatorDefinition = this.opResolver("get");
---
+++ Core.Tests/Utilities/ModelResolvers.cs
@@ -0,0 +1,51 @@
+﻿namespace StatisticsPoland.VtlProcessing.Core.Tests.Utilities
+{
+    using Moq;
+    using StatisticsPoland.VtlProcessing.Core.Infrastructure.DependencyInjection;
+    using StatisticsPoland.VtlProcessing.Core.Models.Interfaces;
+    using StatisticsPoland.VtlProcessing.Core.Models.Logical;
+    using StatisticsPoland.VtlProcessing.Core.Models.Types;
+
+    /// <summary>
+    /// The model resolvers container.
+    /// </summary>
+    public static class ModelResolvers
+    {
+        /// <summary>
+        /// Gets the expression resolver.
+        /// </summary>
+        public readonly static ExpressionResolver ExprResolver;
+
+        /// <summary>
+        /// Gets the data structure resolver.
+        /// </summary>
+        public readonly static DataStructureResolver DsResolver;
+
+        /// <summary>
+        /// Gets the transformation schema resolver.
+        /// </summary>
+        public readonly static TransformationSchemaResolver SchemaResolver;
+
+        /// <summary>
+        /// Initializes resolvers.
+        /// </summary>
+        static ModelResolvers()
+        {
+            Mock<ExpressionResolver> exprResolverMock = new Mock<ExpressionResolver>();
+            exprResolverMock.Setup(o => o(null)).Returns(() => { return new Expression(); });
+            exprResolverMock.Setup(o => o(It.IsNotNull<IExpression>())).Returns((IExpression parentExpr) => { return new Expression(parentExpr); });
+
+            Mock<DataStructureResolver> dsResolverMock = new Mock<DataStructureResolver>();
+            dsResolverMock.Setup(o => o(null, null, null)).Returns(() => { return new DataStructure(); });
+            dsResolverMock.Setup(o => o(It.IsNotNull<string>(), It.IsNotNull<ComponentType?>(), It.IsNotNull<BasicDataType?>()))
+                .Returns((string compName, ComponentType? compType, BasicDataType? dataType) => { return new DataStructure(compName, (ComponentType)compType, (BasicDataType)dataType); });
+
+            Mock<TransformationSchemaResolver> schemaResolverMock = new Mock<TransformationSchemaResolver>();
+            schemaResolverMock.Setup(o => o()).Returns(() => { return new TransformationSchema(); });
+
+            ModelResolvers.ExprResolver = exprResolverMock.Object;
+            ModelResolvers.DsResolver = dsResolverMock.Object;
+            ModelResolvers.SchemaResolver = schemaResolverMock.Object;
+        }
+    }
+}
