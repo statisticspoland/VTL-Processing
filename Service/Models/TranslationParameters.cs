5c6f878d9012a50ea237caeea06a8a76e6a319ce
sklodowskim <m.sklodowski@stat.gov.pl>
2021-11-02 13:44:09 +0100

    Service xml comments and code corrections

--- Service/Models/TranslationParameters.cs
+++ Service/Models/TranslationParameters.cs
@@ -2,12 +2,34 @@
 {
     using System.Collections.Generic;
 
+    /// <summary>
+    /// The parameters of a translation.
+    /// </summary>
     public class TranslationParameters
     {
+        /// <summary>
+        /// Gets or sets the list of data sources.
+        /// </summary>
         public List<DataSource> DataSources { get; set; }
+
+        /// <summary>
+        /// Gets or sets the mapping dictionary.
+        /// </summary>
         public Dictionary<string, string> DataMappers { get; set; }
+
+        /// <summary>
+        /// Gets or sets the target language name.
+        /// </summary>
         public string Target { get; set; }
+
+        /// <summary>
+        /// Gets or sets the VTL 2.0 source expression.
+        /// </summary>
         public string Experession { get; set; }
+
+        /// <summary>
+        /// Gets or sets the default VTL 2.0 namespace name.
+        /// </summary>
         public string DefaultNamespace { get; set; }
     }
 }
